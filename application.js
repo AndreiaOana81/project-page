7808975ea082be01946774ce7c17e4a37c210cc1
AndreiaOana81 <87667349+AndreiaOana81@users.noreply.github.com>
2021-09-30 21:39:52 +0300

    Added "animation" for the hamburger menu

--- application.js
+++ application.js
@@ -2,6 +2,6 @@ let hamburger = document.querySelector(".hamburger");
 let navLinks = document.querySelector(".nav-links");
 let links = document.querySelectorAll (".nav-links li");
 
-hamburger.addEventListener("click ", () => {
+hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
